79650c25dbaeb979552d68ad4eac64eb28dd4321
playerony <pawel.wojtasinski.1995@gmail.com>
2017-02-26 15:03:23 +0100

    Push all

---
+++ android/src/com/izacc/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.izacc.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.izacc.game.Izacc;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Izacc(), config);
+	}
+}
